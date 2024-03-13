0da0a697919e58543a580f8217e8ad290d6775ff
Christian <christian@skjerning.eu>
2024-03-13 20:51:15 +0000

    initial commit for page

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    }
+
+];
